64e557c9c7dd7e13f2fa431f507dd2272b6c96ac
silotech-it <silotech@silotech-it.com.br>
2019-02-06 21:31:07 -0300

    Some testes done

--- VirtualMind/VirtMVC/Models/mvcUserModel.cs
+++ VirtualMind/VirtMVC/Models/mvcUserModel.cs
@@ -10,13 +10,22 @@ namespace VirtMVC.Models
     public class mvcUserModel
     {
         public int id { get; set; }
+
         [DisplayName("FirstName")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Required!!")]
         [MaxLength(50, ErrorMessage = "Até 50 caracteres")]
         public string firstname { get; set; }
+
+        [DisplayName("Lastname")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Required!!")]
+        [MaxLength(50, ErrorMessage = "Até 50 caracteres")]
         public string lastname { get; set; }
+
+        [DisplayName("E-Mail")]
         [EmailAddress]
         public string email { get; set; }
+
+        [DisplayName("Password")]
         public string password { get; set; }
     }
 }
---
+++ VirtualMind/VirtualMind.Tests/Controllers/DolarControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VirtualMind.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMind.Controllers.Tests
+{
+    [TestClass()]
+    public class DolarControllerTests
+    {
+        [TestMethod()]
+        public void GetDolarTest()
+        {
+            VirtualMind.Controllers.DolarController dolar = new DolarController();
+            var currencies = dolar.GetDolar();
+            Assert.AreNotEqual(currencies.Count(), 0);
+        }
+    }
+}
